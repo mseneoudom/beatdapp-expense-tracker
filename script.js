8bd0eb0771e812b43bb12384e00dff02f4271478
mseneoudom <matt_sen@live.ca>
2020-09-23 00:18:54 -0400

    first commit, app is pretty much done. potentially add more styles to make it more intuitive and visually appealing

---
+++ script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+    //Save the expense into the Database - Input fields --> Firebase Database --> Update to add the key --> Remove previous inputs
+    $('.addExpenseButton').on('click',function(){
+        //Create entry in Firebase Database
+        firebase.database().ref('expenses/'+$('#expenseType').children('option:selected').val()).push(
+            {
+                name:$('#addExpense').val(),
+                type:$('#expenseType').children('option:selected').val(),
+                value:$('#expenseValue').val(),
+            }
+            ).then((snapshot)=>{
+                //adding the key to object to better manipulate
+                let firebaseKey = snapshot.key
+                snapshot.update({key:firebaseKey})
+            }).then(()=>{
+                //Delete previous entry from fields
+                $('#addExpense').val('');
+                $('#expenseValue').val('');
+            })
+    })
+
+    //Get reference for saved objects on Firebase
+    firebase.database().ref('expenses').on('value',(data)=>{
+        let firebaseDB = data.val();
+
+        $('.showExpenses').html(
+            '<h2>Expense List</h2>'+
+            '<div class="row">'+
+                '<h3 class="col-sm-2">Transaction ID</h3>'+
+                '<h3 class="col-sm-2">Expense</h3>'+
+                '<h3 class="col-sm-2">Value</h3>'+
+                '<h3 class="col-sm-2">Category</h3>'+
+
+            '</div>'
+            
+        )
+
+        //Display the expenses in the database to the webpage
+        for (let key in firebaseDB){
+            let expenseType = firebaseDB[key];
+            for (let exp in expenseType){
+            $('.showExpenses').append(
+                '<div class="expenseListItem row">'+
+                    '<h4 class="savedExpenseID col-md-2">'+expenseType[exp]['key']+'</h4>'+
+                    '<h4 class="savedExpenseName col-md-2">'+expenseType[exp]['name']+'</h4>'+
+                    '<h4 class="savedExpenseValue col-md-2">$'+expenseType[exp]['value']+'</h4>'+
+                    '<h4 class="savedExpenseType col-md-2">'+expenseType[exp]['type']+'</h4>'+
+                    '<button id="deleteExpenseButton" class="col-md-1" value="expenses/'+key+'/'+exp+'">Delete</button>'+
+                '</div>'
+            );
+            }
+        }
+    })
+
+    $('.showExpenses').on('click','#deleteExpenseButton',function(){
+        firebase.database().ref($(this).val()).remove();
+    })
+
+    // $('.showExpenses .deleteExpenseButton').on('click',function(){
+    //     alert('this works')
+    // })
+}) 
